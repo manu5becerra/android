6e12261746781334021da3c01e8f5b9fb249f607
DESKTOP-9VN7CK2\Castelao <manudomi65@gmail.com>
2020-09-24 14:01:32 +0200

    ADD: Pantalla de inicio (Esquema de pantalla)

---
+++ app/src/main/java/com/example/gupyou/LoginActivity2.java
@@ -0,0 +1,28 @@
+package com.example.gupyou;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class LoginActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login2);
+    }
+}
+
+
